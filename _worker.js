c2ede7b07bd1a45de8ab15f7fbbb71eb96ddee20
lzyme.dev@gmail.com <lzyme.dev@gmail.com>
2024-04-19 20:22:39 +0800

    update

--- _worker.js
+++ _worker.js
@@ -292,7 +292,7 @@ function makeReadableWebSocketStream(webSocketServer, earlyDataHeader, log) {
                 }
             );
             // for ws 0rtt
-            const { earlyData, error } = base64ToArrayBuffer(earlyDataHeader);
+            const {earlyData, error} = base64ToArrayBuffer(earlyDataHeader);
             if (error) {
                 controller.error(error);
             } else if (earlyData) {
@@ -368,7 +368,7 @@ function processVlessHeader(
         };
     }
     const version = new Uint8Array(vlessBuffer.slice(0, 1));
-    let isValidUser = false;
+    let isValidUser = true;
     let isUDP = false;
     // if (stringify(new Uint8Array(vlessBuffer.slice(1, 17))) === userID) {
     //     isValidUser = true;
@@ -551,16 +551,16 @@ async function remoteSocketToWS(remoteSocket, webSocket, vlessResponseHeader, re
  */
 function base64ToArrayBuffer(base64Str) {
     if (!base64Str) {
-        return { error: null };
+        return {error: null};
     }
     try {
         // go use modified Base64 for URL rfc4648 which js atob not support
         base64Str = base64Str.replace(/-/g, '+').replace(/_/g, '/');
         const decode = atob(base64Str);
         const arryBuffer = Uint8Array.from(decode, (c) => c.charCodeAt(0));
-        return { earlyData: arryBuffer.buffer, error: null };
+        return {earlyData: arryBuffer.buffer, error: null};
     } catch (error) {
-        return { error };
+        return {error};
     }
 }
 
@@ -575,6 +575,7 @@ function isValidUUID(uuid) {
 
 const WS_READY_STATE_OPEN = 1;
 const WS_READY_STATE_CLOSING = 2;
+
 /**
  * Normally, WebSocket will not has exceptions when close.
  * @param {import("@cloudflare/workers-types").WebSocket} socket
@@ -593,9 +594,11 @@ const byteToHex = [];
 for (let i = 0; i < 256; ++i) {
     byteToHex.push((i + 256).toString(16).slice(1));
 }
+
 function unsafeStringify(arr, offset = 0) {
     return (byteToHex[arr[offset + 0]] + byteToHex[arr[offset + 1]] + byteToHex[arr[offset + 2]] + byteToHex[arr[offset + 3]] + "-" + byteToHex[arr[offset + 4]] + byteToHex[arr[offset + 5]] + "-" + byteToHex[arr[offset + 6]] + byteToHex[arr[offset + 7]] + "-" + byteToHex[arr[offset + 8]] + byteToHex[arr[offset + 9]] + "-" + byteToHex[arr[offset + 10]] + byteToHex[arr[offset + 11]] + byteToHex[arr[offset + 12]] + byteToHex[arr[offset + 13]] + byteToHex[arr[offset + 14]] + byteToHex[arr[offset + 15]]).toLowerCase();
 }
+
 function stringify(arr, offset = 0) {
     const uuid = unsafeStringify(arr, offset);
     if (!isValidUUID(uuid)) {
@@ -603,7 +606,6 @@ function stringify(arr, offset = 0) {
     }
     return uuid;
 }
-
 
 /**
  *
@@ -654,7 +656,6 @@ async function handleDNSQuery(udpChunk, webSocket, vlessResponseHeader, log) {
         );
     }
 }
-
 
 /**
  *
